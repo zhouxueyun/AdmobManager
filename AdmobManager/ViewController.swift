c91255e4607d8a3274175576fcfae615a7ae22da
zhouxueyun <zhouxueyun@corp.netease.com>
2018-09-29 14:27:53 +0800

    Initial Commit

---
+++ AdmobManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdmobManager
+//
+//  Created by zhouxueyun on 2018/9/29.
+//  Copyright © 2018 zhouxueyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
